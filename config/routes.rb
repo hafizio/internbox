e5286ee07f0d3e18ef35a0cca394e22f1d382f8d
Hafiz <ibnukamy@gmail.com>
2014-07-19 17:56:03 +0800

    Custom url for admins sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'projects#index'
-  devise_for :admins
+  devise_for :admins, path_names: { sign_up: 'secret_passage' }
   devise_for :users
   resources :projects
   get 'tags/:tag', to: 'projects#index', as: :tag
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,32 @@
-#Seed for User still not working due to Devise complication
-#user = User.find_or_create_by(name: 'Hafiz',
-            #email: 'ibnukamy@gmail.com',
-            #encrypted_password: "$2a$10$PVHF3I84VxDHSBbxKslKLuD47brT7OgXLYopZyAiRG4.1mWY00056",
-            #github_account: 'ibnukamy',
-            #twitter_account: 'ibnukamy'
-            #)
+# Seed for User still not working due to Devise complication
+admin = Admin.find_or_initialize_by(name: 'Clarence',
+                                email: 'clarence@tinkerbox.co.sg',
+                                password: 'adminpass12345',
+                                password_confirmation: 'adminpass12345',
+                                github_account: 'clarence',
+                                twitter_account: 'clarence',
+                                availability: true
+                                )
+# admin.skip_confirmation!
+admin.save!
 
-project = Project.find_or_create_by(name: 'Internbox',
+user = User.find_or_initialize_by(name: 'Hafiz',
+            email: 'ibnukamy@gmail.com',
+            password: 'userpass12345',
+            password_confirmation: 'userpass12345',
+            github_account: 'ibnukamy',
+            twitter_account: 'ibnukamy',
+            mentor_id: admin.id
+            )
+# user.skip_confirmation!
+user.save!
+
+project = Project.find_or_initialize_by(name: 'Internbox',
                                     github_url: 'https://github.com/ibnukamy/internbox',
                                     mentor_role: 'Pair Programmer',
                                     project_level: 'Intermediate',
                                     description: 'Application to enchance interns education perfomance',
                                     user_id: 1
-                                    )         
-
-#Incomplete seed
-Tag.find_or_create_by(name: 'intern')
+                                    )
+# project.skip_confirmation!
+project.save!
